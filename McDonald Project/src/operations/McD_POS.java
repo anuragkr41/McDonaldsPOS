6b297a4830a10ba1b68aa61bf88fabeed5068bc8
saurabhkawatra <saurabh.kawatra1@gmail.com>
2021-04-18 17:07:27 +0530

    Created the structure of POS package

--- McDonald Project/src/operations/McD_POS.java
+++ McDonald Project/src/operations/McD_POS.java
@@ -1,9 +1,81 @@
 package operations;
+import java.util.*;
 
+class itemFromManager
+{
+	
+}
+class counterPOS
+{
+	void additem()
+	 {
+		 
+	 }
+	void removeItem()
+	{
+		
+	}
+	void  showTotal()
+	{
+		
+	}
+}
+class Items
+{
+String	itemname;
+int itemprice;
+enum Size{ Big,Medium,Small }
+   Size s;		
+
+}
+class Burger extends Items
+{
+	itemname="MaharajaBurger";
+	itemprice = 320;
+}
+class Drinks extends Items
+{
+	
+}
+class wrap extends Items
+{
+	
+}
+class Meals extends Items
+{
+	
+}
+class iceCream extends Items
+{
+	
+}
 public class McD_POS {
 
 	public static void main(String[] args) {
-System.out.println("Project initiated");
+           System.out.println("Project initiated");
+           System.out.println("Enter the choice");
+           System.out.println("1. Manager");
+           System.out.println("2. Counter Attendant");
+           System.out.println("3. Exit Application");
+           Scanner s=new Scanner(System.in);
+           int choice=s.nextInt();s.close();
+           
+           if(choice==1)
+           {
+        	   
+           }
+           else if(choice==2)
+           {
+        	   
+           }
+           else if(choice==3)
+           {
+        	   
+           }
+           else
+           {
+           System.out.println("Invalid Choice");
+           }
 
 	}
 
